4b23439b256782b25db3da351a281a0d2fc06761
Rankoe1 <sr24020025@biust.ac.bw>
2026-08-30 18:21:38 +0200

    Question 2.  Primitive Playground

---
+++ LAB1/Question2.java
@@ -0,0 +1,22 @@
+public class Question2 {
+    public static void main(String[]args){
+        byte exampleOfByte = 100 ;
+        short exampleOfShort = 20000 ;
+        int exampleOfInt = 350000 ;
+        long exampleOfLong = 900000000;
+        float exampleOfFloat =  3.14f ;
+        double exampleOfDouble = 3.14159265 ;
+        char exampleOfCharacter = 'R' ;
+        boolean exampleOfBoolean = true;
+
+        System.out.println("Byte: " + exampleOfByte);
+        System.out.println("short: " + exampleOfShort);
+        System.out.println("int: " + exampleOfInt);
+        System.out.println("long: " + exampleOfLong);
+        System.out.println("Float: " + exampleOfFloat);
+        System.out.println("Double: " + exampleOfDouble);
+        System.out.println("Char: " + exampleOfCharacter);
+        System.out.println("Boolean: " + exampleOfBoolean);
+    }
+}
+
