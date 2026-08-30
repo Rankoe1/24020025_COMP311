f7d0d29bc2ed83adc62320989c44a92bfc73e923
Rankoe1 <sr24020025@biust.ac.bw>
2026-08-30 18:19:36 +0200

    Question 1.  Personal Profile

---
+++ LAB1/Question1.java
@@ -0,0 +1,14 @@
+public class  Question1{
+    static String name = "Rankoe" ;
+    static int age = 21 ;
+    static double GPA  = 3.2 ;
+    static boolean fullTime = true ;
+
+    public static void main (String[] args){
+        System.out.println("Name: " + name);
+        System.out.println("Age:" + age);
+        System.out.println("GPA: " + GPA);
+        System.out.println("Full-Time:" + fullTime);
+
+    }
+}
